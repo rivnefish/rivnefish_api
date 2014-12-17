210c2fa7134b6e8ce94f90a4ebef05ef48d65fac
Alex K <dev.rivne@gmail.com>
2014-12-17 12:25:25 +0200

    Added staticfiles_urlpatterns to urls

--- RivneFish/urls.py
+++ RivneFish/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -7,3 +8,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
