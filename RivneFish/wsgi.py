6d2d155cdb55c00e481b978443bdadee9873c7d3
Alex K <dev.rivne@gmail.com>
2014-12-17 11:51:29 +0200

    Update wsgi.py, add paths to project.

--- RivneFish/wsgi.py
+++ RivneFish/wsgi.py
@@ -8,6 +8,14 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+
+base = os.path.dirname(os.path.dirname(__file__))
+base_parent = os.path.dirname(base)
+sys.path.append(base)
+sys.path.append(base_parent)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RivneFish.settings")
 
 from django.core.wsgi import get_wsgi_application
