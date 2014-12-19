83e40858e3123749d11a9990e42afa3281c49afb
Alex K <dev.rivne@gmail.com>
2014-12-19 14:39:46 +0200

    Update project urls

--- RivneFish/urls.py
+++ RivneFish/urls.py
@@ -4,7 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    url(r'^api/', include('site_manager.urls')),
+    url(r'^', include('site_manager.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
